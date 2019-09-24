c0e8c4499b43bfb5492658a1aebda2cd2c447519
Alyssa Yates <bosslys@icloud.com>
2019-09-24 18:35:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,17 @@
 class Artist
   attr_accessor :name
 
+  @@all = []
+
   def initialize(name)
     @name = name
     @songs = []
+    @@all << self 
   end
+
+  def self.all 
+    @@all 
+  end 
 
   def add_song(song)
     @songs << song
