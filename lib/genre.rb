23bc0d92b11e67fd5f0f9de980d9fcd31075df1a
Alyssa Yates <bosslys@icloud.com>
2019-09-24 18:58:30 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -3,15 +3,22 @@ class Genre
   attr_reader :name, :genre
   attr_accessor :song
 
+  @@all = []
+
   def initialize(name)
     @name = name
+    @@all << self
   end
 
+
   def songs
-    self.all 
     Song.all.select do |song|
       song.genre == self
     end
+  end
+
+  def self.all
+    @@all 
   end
 
   def artists
