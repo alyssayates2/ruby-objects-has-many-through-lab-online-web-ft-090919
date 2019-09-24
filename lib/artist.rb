d92ae045cd3a575e8ed3210ea9b34147c617e0c0
Alyssa Yates <bosslys@icloud.com>
2019-09-24 18:57:00 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,24 +1,33 @@
 class Artist
-  attr_accessor :name
 
-  @@all = []
+  attr_accessor :name, :artist, :genre
+
+  ALL=[]
 
   def initialize(name)
     @name = name
-    @songs = []
-    @@all << self
+    ALL << self
   end
 
   def self.all
-    @@all
+    ALL
   end
 
-  def new_song(song)
-    @songs << song
-    song.artist = self
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
   end
 
   def songs
-    @songs
+    Song.all.select do |song|
+      song.artist == self
+    end
   end
+
+  def genres
+    Song.all.map do |song|
+      song.genre
+    end
+  end
+
 end
