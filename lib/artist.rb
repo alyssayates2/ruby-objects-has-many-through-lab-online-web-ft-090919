4619be873166232309e607d4e94f0fb398a3736d
Alyssa Yates <bosslys@icloud.com>
2019-09-24 18:35:35 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,11 +6,11 @@ class Artist
   def initialize(name)
     @name = name
     @songs = []
-    @@all << self 
+    @@all << self
   end
 
-  def self.all 
-    @@all 
+  def self.all
+    @@all
   end 
 
   def add_song(song)
