15abcbafece77e06887eef91577c1a71a43cfb6a
Alyssa Yates <bosslys@icloud.com>
2019-09-24 19:16:36 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -16,4 +16,17 @@ class Doctor
   def new_appointment(patient, date)
     Appointment.new(patient, self, date)
   end
+
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+
+  def patients
+    appointments.map do |appointment|
+      appointment.patients
+    end
+  end
+
 end
