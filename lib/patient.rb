6b5f6396f41534f421432b39bd8af8604caa3f93
Alyssa Yates <bosslys@icloud.com>
2019-09-24 19:18:31 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,14 +1,21 @@
 class Patient
 
-  attr_reader :name, :appointment, :doctor
+  attr_reader :name, :appointment, :doctor, :all
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
   end
 
   def new_appointment(doctor, date)
     Appointment.new(self, doctor, date)
   end
+
+  def self.all
+    @@all
+  end 
 
   def appointments
     Appointment.all.select do |appointment|
